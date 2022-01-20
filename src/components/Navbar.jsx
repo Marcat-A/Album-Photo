c640a6fd9a10e4e3054405fefb54069d7f939f0f
MarcatDev <anthony.marcato@gmail.com>
2022-01-20 17:04:53 +0100

    Import/Supr & Add Navbar Default

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import "../styles/index.scss";
 
 function Navbar() {
-  return <div>Nav</div>;
+  return (
+    <div className="navbar container">
+      <a href="#!" className="logo">
+        A<span>lb</span>um Photo
+      </a>
+      <div className="nav-links">
+        <a href="/album">Albums</a>
+        <a href="#!">Catégorie 2</a>
+        <a href="#!">Options</a>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
