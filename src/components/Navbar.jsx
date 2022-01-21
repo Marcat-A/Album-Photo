7f3d3318d3d54bdb15ae20a6ab2f743a55743ea9
MarcatDev <anthony.marcato@gmail.com>
2022-01-21 13:14:24 +0100

    NavBar & Sidebar v1 Finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,59 @@
 import React from "react";
 import { useState } from "react";
+import Sidebar from "./Sidebar";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faImage, faCog } from "@fortawesome/free-solid-svg-icons";
 import "../styles/index.scss";
 
 function Navbar() {
   const [showSidebar, setShowSidebar] = useState(false);
+  const links = [
+    {
+      name: "Accueil",
+      path: "#!",
+      icon: faHome,
+    },
+    {
+      name: "Album",
+      path: "#!",
+      icon: faImage,
+    },
+    {
+      name: "Options",
+      path: "#!",
+      icon: faCog,
+    },
+  ];
+  function closeSidebar() {
+    setShowSidebar(false);
+  }
 
   return (
-    <div className="navbar container">
-      <a href="#!" className="logo">
-        A<span>lb</span>um Photo
-      </a>
-      <div className="nav-links">
-        <a href="/album">Albums</a>
-        <a href="#!">Catégorie 2</a>
-        <a href="#!">Options</a>
+    <>
+      <div className="navbar container">
+        <a href="#!" className="logo">
+          Alb<span>u</span>m Ph<span>o</span>t<span>o</span>
+        </a>
+        <div className="nav-links">
+          {links.map((link) => (
+            <a href={link.path} key={link.name}>
+              <FontAwesomeIcon icon={link.icon} />
+              {link.name}
+            </a>
+          ))}
+        </div>
+        <div
+          onClick={() => setShowSidebar(true)}
+          className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}
+        >
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
       </div>
-      <div
-        onClick={() => setShowSidebar(!showSidebar)}
-        className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}
-      >
-        <div className="bar"></div>
-        <div className="bar"></div>
-        <div className="bar"></div>
-      </div>
-    </div>
+      {showSidebar && <Sidebar close={closeSidebar} links={links} />}{" "}
+    </>
   );
 }
 
